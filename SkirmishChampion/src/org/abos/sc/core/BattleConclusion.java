eb16d5f81f7d204cc102fd42d178f8e9314e5a5c
Ayutac <skoch02@students.uni-mainz.de>
2020-12-05 18:28:59 +0100

    fully commented

--- SkirmishChampion/src/org/abos/sc/core/BattleConclusion.java
+++ SkirmishChampion/src/org/abos/sc/core/BattleConclusion.java
@@ -1,16 +1,29 @@
 package org.abos.sc.core;
 
 /**
+ * An enumeration for battle outcomes.
  * @author Sebastian Koch
  * @version %I%
  * @since 0.1
+ * @see #WON
+ * @see #LOST
+ * @see #TIE
  */
 public enum BattleConclusion {
 	
+	/**
+	 * Indicates that the battle was won.
+	 */
 	WON,
 	
+	/**
+	 * Indicates that the battle was lost.
+	 */
 	LOST,
 	
+	/**
+	 * Indicates that a tie occured.
+	 */
 	TIE;
 
 }
